241049014fe9023a1830f2acd7eff2a6500823f3
Adam Cobbold <adam@tracycobbold.plus.com>
2020-04-02 14:54:58 +0100

    cool stuff

--- main.py
+++ main.py
@@ -10,13 +10,28 @@ window = Tk()
 c = Canvas(window, width = 360, height = 640, bg='black')
 c.pack()
 
-print(allShapes[0].points)
+import location
+gridThisLocation = gridMath.latLongToGrid(location.getLatLong())
 
-for i in range(shapeCount):
-    currentPoints = allShapes[i].points
-    pointCount = len(currentPoints)
 
-    for p in range(pointCount - 1):
-        c.create_line(currentPoints[p][0], currentPoints[p][1], currentPoints[p + 1][0], currentPoints[p + 1][1], fill='lightblue')
+myRoads = shapefile.Reader("data/TQ_RoadLink.shp")
+allShapesRoads = myRoads.shapes()
+shapesInRoads = len(allShapesRoads)
+
+for i in range(shapesInRoads):
+	points = allShapesRoads[i].points
+	pointcount = len(points)
+
+	for p in range(pointcount - 1):
+		x1 = points[p][0] / 100 - gridThisLocation[0] / 100
+		y1 = points[p][1] / 100 - gridThisLocation[1] / 100
+
+		x2 = points[p + 1][0] / 100 - gridThisLocation[0] / 100
+		y2 = points[p + 1][1] / 100 - gridThisLocation[1] / 100
+
+		c.create_line(x1, y1, x2, y2, fill='blue')
+
+
+
 
 c.mainloop()
